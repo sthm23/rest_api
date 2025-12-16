92543c220fe703c096f7a6dec01f215d02ea4f49
Sanjar Tukhtamishev <sanjar_tukhtamishev@epam.com>
2025-12-16 14:10:25 +0500

    feat: add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -14,6 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
